d021f78ee2841f18710a8e265528447a817609ec
Jacob Hummer <jcbhmr@outlook.com>
2023-12-17 14:01:34 -0600

    try revamp shell script

--- install.sh
+++ install.sh
@@ -1,81 +1,65 @@
 #!/bin/sh
-# Copyright 2019 the Deno authors. All rights reserved. MIT license.
-# TODO(everyone): Keep this script simple and easily auditable.
-# Forked from Deno's install.sh script
-
+# ⚠️ Keep this script simple and easily auditable!
 set -e
-
-use_unzip=false
 
 if [ "$OS" = "Windows_NT" ]; then
 	target="x86_64-pc-windows-msvc"
-	use_unzip=true
 else
 	case $(uname -sm) in
-	"Darwin x86_64") target="x86_64-apple-darwin" ;;
-	"Darwin arm64")
-		echo "Error: Official Typst builds for Darwin arm64 are not available." 1>&2 # (see: https://github.com/denoland/deno/issues/1846 )" 1>&2
-		exit 1
-		;;
-	"Linux aarch64")
-		echo "Error: Official Typst builds for Linux aarch64 are not available." 1>&2 # (see: https://github.com/denoland/deno/issues/1846 )" 1>&2
-		exit 1
-		;;
-	*) target="x86_64-unknown-linux-gnu" ;;
+	"Darwin x86_64") target="x86_64-apple-darwin";;;
+	"Darwin arm64") target="aarch64-apple-darwin";;
+	"Linux aarch64") target="aarch64-unknown-linux-musl";;
+	*) target="x86_64-unknown-linux-gnu";;
 	esac
 fi
 
-if [ "$use_unzip" = true ] && ! command -v unzip >/dev/null; then
-	echo "Error: unzip is required to install Typst." 1>&2 # (see: https://github.com/denoland/deno_install#unzip-is-required )." 1>&2
-	exit 1
+if [ "$OS" = "Windows_NT" ]; then
+	archive_ext=".zip"
+else
+  archive_ext=".tar.xz"
 fi
+
+folder="typst-$target"
+file="$folder$archive_ext"
 
 typst_install="${TYPST_INSTALL:-$HOME/.typst}"
-bin_dir="$typst_install/bin"
-exe="$bin_dir/typst"
+mkdir -p "$typst_install"
 
-if [ ! -d "$bin_dir" ]; then
-	mkdir -p "$bin_dir"
-fi
-
-if [ "$use_unzip" = true ]; then
-	if [ $# -eq 0 ]; then
-		typst_uri="https://github.com/typst/typst/releases/latest/download/typst-${target}.zip"
-	else
-		typst_uri="https://github.com/typst/typst/releases/download/${1}/typst-${target}.zip"
- 	fi
-
-	curl --fail --location --progress-bar --output "$exe.zip" "$typst_uri"
-	unzip -d "$bin_dir" -o "$exe.zip"
-	mv "$bin_dir/typst-${target}/*" "$bin_dir"
-	chmod +x "$exe"
-	rm -r "$bin_dir/typst-${target}/*"
-	rm "$exe.zip"
+if [ -n "$1" ]; then
+  url="https://github.com/typst/typst/releases/latest/download/$file"
 else
-	if [ $# -eq 0 ]; then
-		typst_uri="https://github.com/typst/typst/releases/latest/download/typst-${target}.tar.gz"
-	else
-		typst_uri="https://github.com/typst/typst/releases/download/${1}/typst-${target}.tar.gz"
-	fi
-
-	curl --fail --location --progress-bar --output "$exe.tar.gz" "$typst_uri"
-	tar -xf "$exe.tar.gz" -C "$bin_dir" --strip-components=1
-	chmod +x "$exe"
-	rm "$exe.tar.gz"
+  url="https://github.com/typst/typst/releases/download/$1/$file"
 fi
 
-echo "Typst was installed successfully to $exe"
+curl -fsSL "$url" -o "$typst_install/$file"
+if [ "$archive_ext" = ".zip" ]; then
+  unzip -d "$typst_install" -o "$file"
+else
+  tar -xJf "$file" -C "$typst_install"
+fi
+
+mkdir -p "$typst_install/bin"
+mv -f "$typst_install/$folder/typst" "$typst_install/bin/typst"
+chmod +x "$typst_install/bin/typst"
+
+mv -f "$typst_install/$folder"/* "$typst_install"
+rm -rf "$typst_install/$folder"
+
+echo "Typst installed to $typst_install/bin/typst"
+
 if command -v typst >/dev/null; then
 	echo "Run 'typst --help' to get started"
 else
 	case $SHELL in
-	/bin/zsh) shell_profile=".zshrc" ;;
-	*) shell_profile=".bashrc" ;;
+    /bin/zsh) shell_profile=".zshrc";;
+    *) shell_profile=".bashrc";;
 	esac
-	echo "Manually add the directory to your \$HOME/$shell_profile (or similar)"
-	echo "  export TYPST_INSTALL=\"$typst_install\""
-	echo "  export PATH=\"\$TYPST_INSTALL/bin:\$PATH\""
-	echo "Run '$exe --help' to get started"
+  cat <<EOF
+Manually add the directory to your \$HOME/$shell_profile (or similar)
+  export TYPST_INSTALL=\"$typst_install\"
+  export PATH=\"\$TYPST_INSTALL/bin:\$PATH\"
+Run '$exe --help' to get started
+EOF
 fi
-echo
-echo "Stuck? Join our Discord https://discord.gg/2uDybryKPe"
+
+echo "Suck? Open an Issue https://github.com/typst-community/install_typst/issues"
